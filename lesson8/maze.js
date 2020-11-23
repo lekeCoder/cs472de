c33d6171eba078297a9cd3c53d95995f2271b19f
Adeola Adeleke <apps@simplydwift.com>
2020-11-22 18:23:30 -0600

    lesson 8

--- lesson8/maze.js
+++ lesson8/maze.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   $(".boundary").mouseover(function () {
     $(".boundary").addClass("youlose");
+    $(".boundary").attr("disabled", true);
     setTimeout(function () {
       alert("You lose!");
     }, 1000);
@@ -8,6 +9,7 @@ $(document).ready(function () {
 
   $("#start").click(function () {
     $(".boundary").removeClass("youlose");
+    $(".boundary").attr("disabled", false);
   });
 
   $("#end").mouseenter(function () {
